63dda078572782ecbb59d3882d152c777a9c7659
Yohannes Haile <yohannessileshi1@gmail.com>
2025-02-19 22:08:39 -0600

    done with lab1

--- lab1/lab1/src/main/java/edu/miu/Main.java
+++ lab1/lab1/src/main/java/edu/miu/Main.java
@@ -1,5 +1,9 @@
 package edu.miu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     static int gcd(int a, int b) {
@@ -8,14 +12,42 @@ public class Main {
         //loop until the least element of the two is reached
         for (int i = 1; i <= (a < b ? a : b); i++) {
             if (a % i == 0 && b % i == 0) {
-        //when a common divisor is found save it to the variable x
+                //when a common divisor is found save it to the variable x
                 x = i;
             }
         }
         return x;
     }
 
-    public static void main(String[] args) {
-        System.out.println(gcd(9, 6));
-    }
+        public static List<Integer> subsetSum(List<Integer> S, int k) {
+            List<List<Integer>> subsets = new ArrayList<>();
+            subsets.add(new ArrayList<>()); // Start with the empty subset
+            for (int num : S) {
+                List<List<Integer>> newSubsets = new ArrayList<>();
+
+                for (List<Integer> subset : subsets) {
+                    List<Integer> newSubset = new ArrayList<>(subset);
+                    newSubset.add(num);
+
+                    // Check if this new subset sums to k
+                    if (newSubset.stream().mapToInt(Integer::intValue).sum() == k) {
+                        return newSubset;
+                    }
+
+                    newSubsets.add(newSubset);
+                }
+
+                subsets.addAll(newSubsets); // Add new subsets to the list
+            }
+            return null; // No subset found
+        }
+
+        public static void main(String[] args) {
+            System.out.println(gcd(9, 6));
+            List<Integer> S = Arrays.asList(3, 34, 4, 12, 5, 2);
+            int k = 9;
+            List<Integer> subset = subsetSum(S, k);
+            System.out.println(subset != null ? "Subset found: " + subset : "No subset found");
+        }
+
 }
